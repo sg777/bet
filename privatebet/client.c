4ab8bfb0ede59d0f49d353e16eef6a85d333547f
sg777 <sarath.ginjupalli89@gmail.com>
2019-09-23 23:04:07 +0530

    debugging

--- privatebet/client.c
+++ privatebet/client.c
@@ -3088,8 +3088,9 @@ void BET_p2p_clientloop_test(void * _ptr)
         {
         		ptr=0;
 	        	recvlen= nn_recv (bet->subsock, &ptr, NN_MSG, 0);
+				char *tmp=NULL;
 				if(recvlen>0)
-					char *tmp=clonestr(ptr);
+					tmp=clonestr(ptr);
                 if (( (msgjson= cJSON_Parse(tmp)) != 0 ) && (recvlen>0))
                 {
                     if ( BET_p2p_clientupdate_test(msgjson,bet,Player_VARS_global) < 0 )
