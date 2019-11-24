5eaf044ec65b7a540153c0a06a73960e0c4cfd2c
gsaratch <gsaratch@visteon.com>
2019-11-24 18:53:59 +0530

    debugging

--- privatebet/cashier.h
+++ privatebet/cashier.h
@@ -4,3 +4,4 @@ int32_t BET_send_status(struct cashier *cashier_info);
 int32_t BET_cashier_backend(cJSON *argjson,struct cashier *cashier_info);
 void BET_cashier_server_loop(void * _ptr);
 void BET_cashier_client_loop(void * _ptr);
+void BET_cashier_status_loop(void * _ptr);
