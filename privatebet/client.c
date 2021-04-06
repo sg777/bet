874ac13a5a7b67e0f3f2ac6f882182597a8494ea
sg777 <sarath.ginjupalli89@gmail.com>
2021-04-06 11:49:14 +0530

    making changes related to ad_card testing

--- privatebet/client.c
+++ privatebet/client.c
@@ -610,11 +610,13 @@ end:
 
 void display_cards()
 {
+	#if 0
 	char *cards[52] = { "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
 			    "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
 			    "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
 			    "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS" };
 
+	#endif
 	cJSON *init_card_info = NULL, *hole_card_info = NULL, *init_info = NULL, *board_card_info = NULL;
 
 	init_info = cJSON_CreateObject();
